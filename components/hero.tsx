6aca33868734f8583957174d81cdb8f734c4e794
jucornejo-debug <jucornejo@fi.uba.ar>
2026-01-04 16:25:24 -0300

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 export function Hero() {
   return (
-    <section className="relative h-screen min-h-[710px] flex items-center justify-center overflow-hidden">
+    <section className="relative h-screen min-h-[705px] flex items-center justify-center overflow-hidden">
       
       {/* Background Image */}
       <div className="absolute inset-0">
