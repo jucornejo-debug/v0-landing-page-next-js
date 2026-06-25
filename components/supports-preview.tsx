bdede48cb6522839018a7715b9594fc9db1916c7
jucornejo-debug <jucornejo@fi.uba.ar>
2026-06-25 20:10:08 -0300

    Update supports-preview.tsx

--- components/supports-preview.tsx
+++ components/supports-preview.tsx
@@ -18,7 +18,7 @@ export function SupportsPreview() {
     {
       title: "FORMATOS DIGITALES",
       description: "Pantallas digitales en puntos estratégicos de gran circulación.",
-      image: "/Imagen Tótems (2)",
+      image: "/Imagen Tótems (2).jpg",
     },
   ]
 
